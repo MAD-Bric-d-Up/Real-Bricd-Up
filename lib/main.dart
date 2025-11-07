94981be193c9a719e54bb45181cb9b7756553b5c
Caleb C <kaichun24@gmail.com>
2025-11-07 11:19:41 -0800

    firebase added in and i just pushed some rando stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:bricd_up/pages/camera.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:bricd_up/pages/login.dart';
 
 void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   WidgetsFlutterBinding.ensureInitialized();
 
